7605ce58e371db3b8cbdc8211de84ef513c93e91
A-J-Sarmah <devajsarmah@gmail.com>
2022-04-10 21:00:04 +0530

    Started working on collision detection

--- src/app.js
+++ src/app.js
@@ -1,16 +1,25 @@
-import {initialCanvas,eatFood,displayFood,draw,clearCanvas,locateFood,checkIsGameOver,calculateSnakePosition} from "./utils.js";
+import {
+  initialCanvas,
+  eatFood,
+  displayFood,
+  draw,
+  clearCanvas,
+  locateFood,
+  checkIsGameOver,
+  calculateSnakePosition,
+} from "./utils.js";
 
 //game variables
 let is_playing = false;
-let snake_position = [3,2,1];
+let snake_position = [3, 2, 1];
 //directions can be 'N' , 'S' , 'E' , 'W' denoting North , South , East , West respectively.
-let direction = 'E';
+let direction = "E";
 let next_head_position = 4;
 let is_food = false;
-let food_location = undefined
+let food_location = undefined;
 const game_speed = 200; //in miliseconds
 
-const canvas = document.querySelector('.canvas');
+const canvas = document.querySelector(".canvas");
 /*
 	we know width of our canvas = 600px
 	In our css we specified width of our each block to be 15px
@@ -31,77 +40,92 @@ const canvas = document.querySelector('.canvas');
 	So total height of canvas is found to be 300px
 */
 const no_of_box_in_each_row = 40;
-const no_of_row = 20
+const no_of_row = 20;
 
-initialCanvas(no_of_box_in_each_row,no_of_row,canvas)
-
+initialCanvas(no_of_box_in_each_row, no_of_row, canvas);
 
 //starting the game
-window.addEventListener("keydown",event=>{
-	if(!is_playing)
-	{
-		is_playing = true;
-		window.requestAnimationFrame(main);
-	}
-})
+window.addEventListener("keydown", (event) => {
+  if (!is_playing) {
+    is_playing = true;
+    window.requestAnimationFrame(main);
+  }
+});
 
 //handling user input and starting game
 function handleUserInput(event) {
-	let key = event.key.toUpperCase();
-	if (key === "W" && direction !== "N" && direction !== "S") {
-		direction = "N";
-		next_head_position = snake_position[0] - 40;
-	} else if (key === "S" && direction !== "N" && direction !== "S") {
-		direction = "S";
-		next_head_position = snake_position[0] + 40;
-	} else if (key === "A" && direction !== "W" && direction !== "E") {
-		direction = "W";
-		 next_head_position = snake_position[0] - 1;
-	} else if (key === "D" && direction !== "W" && direction !== "E") {
-		direction = "E";
-		next_head_position = snake_position[0] + 1;
-	}
+  let key = event.key.toUpperCase();
+  if (key === "W" && direction !== "N" && direction !== "S") {
+    direction = "N";
+    next_head_position = snake_position[0] - 40;
+  } else if (key === "S" && direction !== "N" && direction !== "S") {
+    direction = "S";
+    next_head_position = snake_position[0] + 40;
+  } else if (key === "A" && direction !== "W" && direction !== "E") {
+    direction = "W";
+    next_head_position = snake_position[0] - 1;
+  } else if (key === "D" && direction !== "W" && direction !== "E") {
+    direction = "E";
+    next_head_position = snake_position[0] + 1;
+  }
 }
 window.addEventListener("keydown", handleUserInput);
 
 //function for restaring game
-function restartGame(food_location,snake_position,direction,next_head_position,is_food) {
-	if(food_location){
-		let element = document.getElementById(food_location)
-		element.classList.remove("food")
-	}
-	for(let i=0;i<snake_position.length;i++)
-	{
-		let element = document.getElementById(snake_position[i])
-		element.classList.remove("snake-body")
-	}
-	snake_position = [3, 2, 1];
-	direction = "E";
-	next_head_position = 4;
-	is_food = false;
-	return[snake_position,direction,next_head_position,is_food]
+function restartGame(
+  food_location,
+  snake_position,
+  direction,
+  next_head_position,
+  is_food
+) {
+  if (food_location) {
+    let element = document.getElementById(food_location);
+    element.classList.remove("food");
   }
+  for (let i = 0; i < snake_position.length; i++) {
+    let element = document.getElementById(snake_position[i]);
+    if (element) element.classList.remove("snake-body");
+  }
+  snake_position = [3, 2, 1];
+  direction = "E";
+  next_head_position = 4;
+  is_food = false;
+  return [snake_position, direction, next_head_position, is_food];
+}
 
 //the game loop (the recursive main function)
-function main(){
-	[snake_position,is_playing] = checkIsGameOver(snake_position)
-	if (is_food) {
-		[snake_position,is_food,food_location] = eatFood(snake_position[0],snake_position,is_food,food_location);
-	  } 
-	else {
-		food_location = locateFood(snake_position)
-		displayFood(food_location);
-		is_food = true;
-	}
-	clearCanvas(snake_position);
-	[next_head_position,snake_position]= calculateSnakePosition(snake_position,next_head_position);
-	draw(snake_position);
-	if (is_playing) {
-		setTimeout(() => {
-		  window.requestAnimationFrame(main);
-		}, game_speed);
-	} 
-	else {
-		[snake_position,direction,next_head_position,is_food] = restartGame(food_location,snake_position,direction,next_head_position,is_food);
-	}
+function main() {
+  [snake_position, is_playing] = checkIsGameOver(snake_position, direction);
+  if (is_food) {
+    [snake_position, is_food, food_location] = eatFood(
+      snake_position[0],
+      snake_position,
+      is_food,
+      food_location
+    );
+  } else {
+    food_location = locateFood(snake_position);
+    displayFood(food_location);
+    is_food = true;
+  }
+  clearCanvas(snake_position);
+  [next_head_position, snake_position] = calculateSnakePosition(
+    snake_position,
+    next_head_position
+  );
+  draw(snake_position);
+  if (is_playing) {
+    setTimeout(() => {
+      window.requestAnimationFrame(main);
+    }, game_speed);
+  } else {
+    [snake_position, direction, next_head_position, is_food] = restartGame(
+      food_location,
+      snake_position,
+      direction,
+      next_head_position,
+      is_food
+    );
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,102 +1,127 @@
 //creating the canvas and drawing and clearing it
-export const initialCanvas = (no_of_box,no_of_row,canvas) => {
-	const total_no_of_box = no_of_box*no_of_row
+export const initialCanvas = (no_of_box, no_of_row, canvas) => {
+  const total_no_of_box = no_of_box * no_of_row;
 
-	//we calculate total no of boxes in the entire canvas and iterate over it
-	//flex wrap in CSS automatically put boxes in new row when 40 boxes are put in a row.
-	
-	for(let i=0; i<total_no_of_box;i++){
-		canvas.innerHTML +=  `<div id=${i} class="box"></div>`;
-	}
-}
+  //we calculate total no of boxes in the entire canvas and iterate over it
+  //flex wrap in CSS automatically put boxes in new row when 40 boxes are put in a row.
+
+  for (let i = 0; i < total_no_of_box; i++) {
+    canvas.innerHTML += `<div id=${i} class="box"></div>`;
+  }
+};
 
 export function draw(snake_position) {
-	for (let i = 0; i < snake_position.length; i++) {
-	  const id = snake_position[i];
-	  const element = document.getElementById(id.toString());
-	  if (element) element.classList.add("snake-body");
-	}
+  for (let i = 0; i < snake_position.length; i++) {
+    const id = snake_position[i];
+    const element = document.getElementById(id.toString());
+    if (element) element.classList.add("snake-body");
   }
-
-  export function clearCanvas(snake_position) {
-	for (let i = 0; i < snake_position.length; i++) {
-	  const id = snake_position[i];
-	  const element = document.getElementById(id.toString());
-	  if (element) element.classList.remove("snake-body");
-	}
-  }
-
-//checking if game is over
-export function checkIsGameOver(snake_position)
-{
-	//for checking if the game is over we must check 2 things
-	// 1. if snake bites itself
-	// 2. if snake's head collided with the walls
-	let is_playing = true;
-	const snake_head = snake_position[0]
-	//checking condition 1
-	// P.S. : We are iterating from i = 1 as 0 is the head of the snake and from 1 is where the bosy of snake begins so if snakes head collide with it's own body then it is dead.
-	for(let i=1;i<snake_position.length;i++)
-	{
-		if(snake_position[i] === snake_head){
-			is_playing = false;
-			break;
-		}
-	}
-	return [snake_position,is_playing]
 }
 
+export function clearCanvas(snake_position) {
+  for (let i = 0; i < snake_position.length; i++) {
+    const id = snake_position[i];
+    const element = document.getElementById(id.toString());
+    if (element) element.classList.remove("snake-body");
+  }
+}
+
+//checking if game is over
+export function checkIsGameOver(snake_position, direction) {
+  //for checking if the game is over we must check 2 things
+  // 1. if snake bites itself
+  // 2. if snake's head collided with the walls
+  let is_playing = true;
+  const snake_head = snake_position[0];
+  //checking condition 1
+  // P.S. : We are iterating from i = 1 as 0 is the head of the snake and from 1 is where the bosy of snake begins so if snakes head collide with it's own body then it is dead.
+  for (let i = 1; i < snake_position.length; i++) {
+    if (snake_position[i] === snake_head) {
+      is_playing = false;
+      break;
+    }
+  }
+  //checking condition 2
+  //   const pos_north_south = [];
+  //   for (let i = 0; i < 2; i++) {
+  //     const state = i;
+  //     for (let j = 0; j < 40; j++) {
+  //       if (state === 1) pos_north_south.push(760 + j);
+  //       else pos_north_south.push(j);
+  //     }
+  //   }
+  //   for (let i = 0; i < pos_north_south.length; i++) {
+  //     if (
+  //       snake_head === pos_north_south[i] &&
+  //       (direction === "N" || direction === "S")
+  //     )
+  //       is_playing = false;
+  //   }
+  //   const pos_east_west = [
+  //     0, 40, 80, 120, 160, 200, 240, 280, 320, 360, 400, 440, 480, 520, 560, 600,
+  //     640, 680, 720, 760, 39, 79, 119, 159, 199, 239, 279, 319, 359, 399, 439,
+  //     479, 519, 559, 599, 639, 679, 719, 759, 799,
+  //   ];
+  //   for (let i = 0; i < pos_east_west.length; i++) {
+  //     if (
+  //       (snake_head === pos_east_west[i] && direction === "E") ||
+  //       direction === "W"
+  //     )
+  //       is_playing = false;
+  //   }
+  return [snake_position, is_playing];
+}
 
 // functions associated with fooding of snake
-export function eatFood(head,snake_position,is_food,location) {
-	const element = document.getElementById(location.toString());
-	if (element.classList.contains("food") && location === head) {
-		element.classList.remove("food");
-		is_food = false;
-		location = undefined
-		const size = snake_position[1] - snake_position[0];
-		const add = snake_position[snake_position.length - 1] + size;
-		snake_position.push(add);
-	}
-	return [snake_position,is_food,location]
+export function eatFood(head, snake_position, is_food, location) {
+  const element = document.getElementById(location.toString());
+  if (element.classList.contains("food") && location === head) {
+    element.classList.remove("food");
+    is_food = false;
+    location = undefined;
+    const size = snake_position[1] - snake_position[0];
+    const add = snake_position[snake_position.length - 1] + size;
+    snake_position.push(add);
+  }
+  return [snake_position, is_food, location];
 }
 
 export function locateFood(snake_position) {
-	let foodLocation = 0;
-	let isFound = false;
-	while (!isFound) {
-	  const food = Math.floor(Math.random() * 800);
-	  let state = true;
-	  for (let i = 0; i < snake_position.length; i++) {
-		if (food === snake_position[i]) {
-			state = false;
-			break;
-		}
-	  }
-	  if(state){
-		isFound = true;
-		foodLocation = food;
-	  }
-	}
-	return foodLocation;
+  let foodLocation = 0;
+  let isFound = false;
+  while (!isFound) {
+    const food = Math.floor(Math.random() * 800);
+    let state = true;
+    for (let i = 0; i < snake_position.length; i++) {
+      if (food === snake_position[i]) {
+        state = false;
+        break;
+      }
+    }
+    if (state) {
+      isFound = true;
+      foodLocation = food;
+    }
   }
+  return foodLocation;
+}
 
 export function displayFood(location) {
-	const element = document.getElementById(location.toString())
-	element.classList.add("food");
+  const element = document.getElementById(location.toString());
+  element.classList.add("food");
 }
 
 //claculating position of snake
-export function calculateSnakePosition(snake_position,next_head_position) {
-	let new_snake_position = [];
-	for (let i = 0; i < snake_position.length; i++) {
-	  if (i == 0) {
-		const pos = next_head_position - snake_position[i];
-		new_snake_position.push(next_head_position);
-		next_head_position = next_head_position + pos;
-	  } else {
-		new_snake_position.push(snake_position[i - 1]);
-	  }
-	}
-	return [next_head_position,new_snake_position];
+export function calculateSnakePosition(snake_position, next_head_position) {
+  let new_snake_position = [];
+  for (let i = 0; i < snake_position.length; i++) {
+    if (i == 0) {
+      const pos = next_head_position - snake_position[i];
+      new_snake_position.push(next_head_position);
+      next_head_position = next_head_position + pos;
+    } else {
+      new_snake_position.push(snake_position[i - 1]);
+    }
   }
+  return [next_head_position, new_snake_position];
+}
